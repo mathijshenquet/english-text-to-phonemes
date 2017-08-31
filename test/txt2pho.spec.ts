2e98371c6c5750ff43f3446b8b615bf2994a0347
Maarten <maartentrompper@freedom.nl>
2017-08-31 17:26:25 +0200

    Update txt2pho.spec.ts

--- test/txt2pho.spec.ts
+++ test/txt2pho.spec.ts
@@ -5,7 +5,38 @@ import {expect} from "chai";
 
 
 describe("txt2pho", () => {
+it(
+        "fox"
+        , () => {
+"That quick beige fox jumped in the air over each thin dog. Look out, I shout, for he's foiled you again, creating chaos."
+});
 
+it(
+        "perfect for certain Received Pronunciation accents"
+        , () => {
+"Are those shy Eurasian footwear, cowboy chaps, or jolly earthmoving headgear?"
+});
+
+
+it(
+        "dinosaur"
+        , () => {
+"The hungry purple dinosaur ate the kind, zingy fox, the jabbering crab, and the mad whale and started vending and quacking."
+});
+
+
+
+it(
+        "for certain US accents and phonological analyses"
+        , () => {
+"With tenure, Suzie’d have all the more leisure for yachting, but her publications are no good." 
+});
+
+it("perfect for certain accents with the cot-caught merger", () => {
+"Shaw, those twelve beige hooks are joined if I patch a young, gooey mouth." 
+});
+
+"The beige hue on the waters of the loch impressed all, including the French queen, before she heard that symphony again, just as young Arthur wanted."
         it("should convert trill lyrics", () => {
             expect(
             convert(
